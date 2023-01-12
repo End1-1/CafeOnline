9b8c4b2f3686eee7f92f6bbbdb2a25b939d1d54e
user <end1_1@yahoo.com>
2023-01-12 22:34:58 +0400

    F

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,6 +1,6 @@
 library Const;
 
-List<String> years = <String>['2021', '2022'];
+List<String> years = <String>['2021', '2022', '2023'];
 List<String> months = <String>['Հունվար','Փետրվար','Մարտ','Ապրիլ','Մայիս','Հունիս','Հուլիս','Օգոստոս','Սեպտեմբեր','Հոկտեմբեր','Նոյեմբեր','Դեկտեմբեր'];
 List<String> cafeNames = <String>['Օպերա', 'Թումանյան','Կոմիտաս', 'Աբովյան', 'Արմենիա', 'Երևան Մոլլ'];
 List<String> storeNames = <String>['Բար', 'Խոհանոց', 'Սառնարան'];
